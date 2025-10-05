4356923c7ae13f31f378e16fbbe304b0f13c1a06
Sibu Mjavu <mjavusibulele@gmail.com>
2025-10-05 14:31:55 +0200

    update workspace

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:veriwork_mobile/views/pages/loginScreen.dart';
+=======
+import 'package:veriwork_mobile/views/pages/loginscreen.dart';
+>>>>>>> Stashed changes
 import 'package:veriwork_mobile/views/pages/onboarding_page.dart';
 
 void main() {

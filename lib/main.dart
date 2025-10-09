6f6e362b0bebcaac8990b2bd5a10fbe3015f710e
Damian-Salley <damiansalley@gmail.com>
2025-10-09 13:17:01 +0200

    Selfie page

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,175 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //Loading Images through gallery
+  File? _image;
+  final _picker = ImagePicker();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  pickImage() async {
+    //ImageSource.gallery can be changed to ImageSource.camera to use the camera directly
+    //and avoid the use of the gallery
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(""),
+        backgroundColor: Colors.blue,
+        actions: <IconButton>[
+          IconButton(
+            padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+            icon: Row(
+              children: [
+                //Veriwork Logo
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/emptyProfile.png'),
+                  radius: 25,
+                ),
+
+                //Spacing between Veriwork Logo and Logout Icon
+                SizedBox(width: 80),
+
+                //Logout Icon
+                const Icon(Icons.logout, size: 30, color: Colors.white),
+
+                //Spacing between Logo and Avatar
+                SizedBox(width: 15),
+
+                //Avatar Icon
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/emptyProfile.png'),
+                  radius: 20,
+                ),
+              ],
+            ),
+            onPressed: () {
+              // Perform an action when the settings icon is pressed
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
+      //Space above "Take Photo" text
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 30, 0, 15),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          children: [
+            Center(
+              child: Text(
+                "Take Photo",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+
+            //Text for selfie verification
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Take a clear selfie for verification",
+              style: TextStyle(fontSize: 17),
+            ),
+
+            //Profile Photo Background Widget
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
+              child: Container(
+                width: 500,
+                height: 350,
+                color: const Color.fromARGB(255, 85, 142, 189),
+                child: Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: ClipOval(
+                    child: CircleAvatar(
+                      backgroundColor: const Color.fromARGB(255, 145, 155, 160),
+                      child:
+                          _image == null
+                              ? Icon(Icons.person, size: 200)
+                              : Image.file(_image!, fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            //Button to take a selfie
+            MaterialButton(
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(110, 25, 110, 25),
+                child: const Text(
+                  "Capture Selfie",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              onPressed: () {
+                pickImage();
+              },
+            ),
+
+            //Submit Button
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: MaterialButton(
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(115, 25, 110, 25),
+                  child: const Text(
+                    "Submit Selfie",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                onPressed: () {},
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
